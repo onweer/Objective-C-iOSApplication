fb955301035f64ead5a74bcf03eb7c2836216715
Zongminglee <zongmingli@outlook.com>
2016-01-28 20:43:32 +0800

    add UIViewDemo

---
+++ UIView/UIView/UIView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIView
+//
+//  Created by SpiColorPendra on 16/1/28.
+//  Copyright © 2016年 SpiColorPendra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIView/UIView/UIView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIView
+//
+//  Created by SpiColorPendra on 16/1/28.
+//  Copyright © 2016年 SpiColorPendra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
