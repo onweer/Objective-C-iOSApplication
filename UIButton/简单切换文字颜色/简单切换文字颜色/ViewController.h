1a3e3f271ad11a044541e89d96c5f2df6349393d
Zongminglee <zongmingli@outlook.com>
2016-01-28 17:48:36 +0800

    add font color

--- UIButton/简单切换文字颜色/简单切换文字颜色/ViewController.h
+++ UIButton/简单切换文字颜色/简单切换文字颜色/ViewController.h
@@ -10,6 +10,6 @@
 
 @interface ViewController : UIViewController
 
-
+@property (nonatomic,weak)IBOutlet UILabel *label;
 @end
 
--- UIButton/简单切换文字颜色/简单切换文字颜色/ViewController.m
+++ UIButton/简单切换文字颜色/简单切换文字颜色/ViewController.m
@@ -14,14 +14,29 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+#pragma mark 监听红色按钮点击
+-(IBAction) redClick
+{
+    NSLog(@">>>红色按钮<<<");
+    self.label.textColor = [UIColor redColor];
+    self.label.text = @"一段红色文字";
+    self.label.font = [UIFont systemFontOfSize:15];
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark 监听黄色按钮点击
+-(IBAction) yellowClick
+{
+    NSLog(@">>>黄色按钮<<<");
+    self.label.textColor = [UIColor yellowColor];
+    self.label.text = @"一段黄色文字";
+    self.label.font = [UIFont systemFontOfSize:20];
+}
+#pragma mark 监听蓝色按钮点击
+-(IBAction) blueClick
+{
+    NSLog(@">>>蓝色按钮<<<");
+    self.label.textColor = [UIColor blueColor];
+    self.label.text = @"一段蓝色文字";
+    self.label.font = [UIFont systemFontOfSize:25];
 }
 
 @end
